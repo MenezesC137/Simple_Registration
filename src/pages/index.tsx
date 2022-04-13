393c8f2444268e8d0e0fce821af01ba4d31c81ef
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-04-13 10:44:25 -0300

    Layout & Titulo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,19 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
     <div className={`
+      flex h-screen
+      justify-center
+      items-center
+      bg-gradient-to-r
+      from-blue-400
+      to-blue-700
+      text-white
     `}>
-      <span></span>
+      <Layout titulo="Cadastro Simples">
+        <span>Conteudo</span>
+      </Layout>
     </div>
   )
 }
